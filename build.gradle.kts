37a051f4ac4cdf6dcb1f1baa8148cf4aec84cf2a
Нураттин <hhhhghhghu@gmail.com>
2023-08-24 16:03:42 +0300

    -Added dependencies -Migrate build to version catalogs

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,19 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath(libs.kotlin.serialization)
+        classpath(libs.kotlin.gradle)
+        classpath(libs.google.dagger)
+    }
+}
+
 plugins {
-    id("com.android.application") version "8.1.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.10" apply false
+    alias(libs.plugins.jetbrains.android) apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.library) apply false
 }
